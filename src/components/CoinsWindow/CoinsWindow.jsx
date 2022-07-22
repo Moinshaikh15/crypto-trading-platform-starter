7b0882a77b676c4712b91a884efe8ed7058a1db4
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-22 02:33:18 -0700

    fetch complete, in progress

--- src/components/CoinsWindow/CoinsWindow.jsx
+++ src/components/CoinsWindow/CoinsWindow.jsx
@@ -1,10 +1,14 @@
 import "./styles.css";
+import { useContext } from "react";
+import coinData from "../../contexts/coinData";
 
 function CoinsWindow() {
+  let coinDataContext = useContext(coinData);
+
   return (
-    <div className="coinsWindow">
-      <div></div>
-    </div>
+    <>
+      <div className="coinsWindow">dasdasd{"getData()"}</div>
+    </>
   );
 }
 
--- src/components/UserInterface/UserInterface.jsx
+++ src/components/UserInterface/UserInterface.jsx
@@ -1,19 +1,51 @@
 import "./styles.css";
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import coinData from "../../contexts/coinData";
 import CoinsWindow from "../CoinsWindow/CoinsWindow";
 
+const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20solana&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
 function reducer(state, action) {
   switch (action.type) {
     case "buy":
       break;
     case "sell":
       break;
+    case "dataUpdate":
+      return { ...state, coinsInfo: action.payload };
+    default:
+      break;
   }
 }
 
 function UserInterface() {
-  let [state, dispatch] = useReducer(reducer, { wallet: 100, portfoilio: [], coinInfo: false });
+  let [state, dispatch] = useReducer(reducer, { wallet: 100, portfoilio: [], coinNames: ["bitcoin", "ethereum", "cardano", "solana"], coinsInfo: false });
+
+  async function getData() {
+    console.log("ping");
+    let response = await fetch(URL).catch((error) => {
+      console.log(error);
+    });
+    let data = await response.json();
+
+    let output = {};
+    for (const obj of data) {
+      output[obj.name.toLowerCase()] = { name: obj.name, price: obj.current_price, change24Per: Number(obj.price_change_percentage_24h).toFixed(5), src: obj.image };
+    }
+
+    dispatch({ type: "dataUpdate", payload: output });
+  }
+
+  useEffect(() => {
+    const getInterval = setInterval(() => {
+      getData();
+    }, 5000);
+
+    return () => {
+      clearInterval(getInterval);
+    };
+  }, []);
+
+  console.log(state.coinsInfo);
 
   return (
     <div id="container">
@@ -22,12 +54,12 @@ function UserInterface() {
         <div id="header2">To buy virtual food</div>
         <div id="header3">Wallet: ${state.wallet}</div>
         <div id="header4">Portfolio Value: ${"currentValueOfCoinsIHave"}</div>
-        {state.coinInfo ? (
-          <div>"Fetching..."</div>
-        ) : (
+        {state.coinsInfo.length > 0 ? (
           <coinData.Provider value={{ state, dispatch }}>
             <CoinsWindow />
           </coinData.Provider>
+        ) : (
+          <div>"Fetching..."</div>
         )}
       </div>
     </div>
