ae607a34194b171cd3cf97eab74f28680414d903
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-22 01:03:41 -0700

    added context and reducer partially

--- src/components/UserInterface/UserInterface.jsx
+++ src/components/UserInterface/UserInterface.jsx
@@ -1,9 +1,28 @@
 import "./styles.css";
+import { useReducer } from "react/cjs/react.production.min";
+import coinData from "../../contexts/coinData";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "buy":
+      break;
+    case "sell":
+      break;
+  }
+}
 
 function UserInterface() {
+  let [state, dispatch] = useReducer(reducer, { wallet: 100, portfoilio: [], coinInfo: [] });
+
   return (
-    <div className="userInterface">
-      <div></div>
+    <div id="container">
+      <div id="appHeader">
+        <div id="header1">Earn some virtual money</div>
+        <div id="header2">To buy virtual food</div>
+        <div id="header3">Wallet: ${"currentAmountInWallet"}</div>
+        <div id="header4">Portfolio Value: ${"currentValueOfCoinsIHave"}</div>
+        {}
+      </div>
     </div>
   );
 }
---
+++ src/contexts/coinData.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const coinData = createContext(null);
+
+export default coinData;
